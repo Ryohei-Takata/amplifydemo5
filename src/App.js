68940489ce3f24005689c9397f3b6e7c5169d0c0
SC-HOLDINGS\ryo-takata <ryo-takata@sc-ko.jp>
2023-04-20 18:30:26 +0900

    MUI対応

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
 import "./App.css";
 
+const theme = createTheme();
+
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography variant="h4" component="h1" align="center" gutterBottom>
+          Todo List
+        </Typography>
+        <List>
+          {data.map((item) => (
+            <ListItem key={item.id}>
+              <ListItemText primary={item.title} />
+            </ListItem>
+          ))}
+        </List>
+      </Container>
+    </ThemeProvider>
   );
 }
 
